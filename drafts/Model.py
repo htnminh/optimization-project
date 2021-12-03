3da6a7ffaf66d43b879e51d4a1258594840e1146
htnminh <nhatminh2106@gmail.com>
2021-12-03 09:57:27 +0700

    rename list of list of variables, fix constraints to =1

--- drafts/Model.py
+++ drafts/Model.py
@@ -8,21 +8,21 @@ class Model(cp_model.CpModel):
         self.n_cars = n_cars
         self.n_blocks = n_blocks
         
-        self.contains = list()
+        self.x = list()
 
         self._initialize_variables()
         self._initialize_constraints()
         
     def _initialize_variables(self):
         for c in range(self.n_cars):
-            self.contains.append(list())
+            self.x.append(list())
             
             for b in range(self.n_blocks):
-                self.contains[c].append(self.NewIntVar(1, 1, f"c[{c}][{b}]"))
+                self.x[c].append(self.NewIntVar(1, 1, f"x[{c}][{b}]"))
         
     def _initialize_constraints(self):
         for b in range(self.n_blocks):
             linear_expr = cp_model.LinearExpr.Sum(
-                                [self.contains[c][b] for c in range(self.n_cars)])
-            self.AddLinearConstraint(linear_expr, 0, 1)
+                                [self.x[c][b] for c in range(self.n_cars)])
+            self.AddLinearConstraint(linear_expr, 1, 1)
 
