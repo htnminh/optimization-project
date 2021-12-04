ac03f9899eaab348cdd7ed8a63afb099f281dd90
htnminh <nhatminh2106@gmail.com>
2021-12-04 20:30:42 +0700

    add some functionalities, read description
    add _read_input(), to yeah, read input
    change variable names to understand better what's inside those
    print the model parameters if the file is run as a script

--- drafts/Model.py
+++ drafts/Model.py
@@ -2,16 +2,32 @@ from ortools.sat.python import cp_model
 
 
 class Model(cp_model.CpModel):
-    def __init__(self, n_cars: int, n_blocks: int) -> None:
+    def __init__(self) -> None:
         super().__init__()
         
-        self.n_cars = n_cars
-        self.n_blocks = n_blocks
+        self.n_blocks = None
+        self.n_cars = None
+        self.blocks = list()
+        self.cars = list()
+
+        self._read_input()
         
         self.x: list(list(self.NewIntVar)) = list()
 
         self._initialize_variables()
         self._initialize_constraints()
+
+    def _read_input(self, file_name='drafts\data.txt'):
+        with open(file_name, 'r') as f:
+            self.n_blocks, self.n_cars = map(int, f.readline().split())
+
+            for _ in range(self.n_blocks):
+                self.blocks.append(tuple(map(int, f.readline().split())))
+
+            cars = list() # todo
+            for _ in range(self.n_cars):
+                self.cars.append(tuple(map(int, f.readline().split())))
+
         
     def _initialize_variables(self):
         for c in range(self.n_cars):
@@ -22,8 +38,25 @@ class Model(cp_model.CpModel):
         
     def _initialize_constraints(self):
         for b in range(self.n_blocks):
-            linear_expr = cp_model.LinearExpr.Sum(
+            sum_in_cars = cp_model.LinearExpr.Sum(
                                 [self.x[c][b] for c in range(self.n_cars)])
-            self.AddLinearConstraint(linear_expr, 1, 1)
+            self.Add(sum_in_cars == 1)
         
-        
+        for c in range(self.n_cars):
+            self.NewBoolVar('b')
+            sum_in_blocks = cp_model.LinearExpr.Sum(
+                                [self.x[c][b] for b in range(self.n_blocks)])
+
+if __name__ == '__main__':
+    model = Model()
+    spaces = ' ' * 12
+    print(f'n_blocks =\n{spaces}{model.n_blocks}')
+    print(f'blocks =\n{spaces}{model.blocks}')
+    print(f'n_cars =\n{spaces}{model.n_cars}')
+    print(f'cars =\n{spaces}{model.cars}')
+    print()
+    print(model.ModelStats())
+    
+
+            
+
