6dd807ebc480013d73c306220d60159fe140d23f
htnminh <nhatminh2106@gmail.com>
2021-12-23 19:11:37 +0700

    added figures to represent the dataframe

--- files/_ana_data_load_describe.py
+++ files/_ana_data_load_describe.py
@@ -9,11 +9,13 @@ pd.options.display.max_colwidth = None
 pd.options.display.expand_frame_repr = False
 
 
+output_file_path = 'files/_ana_data_loaded_describe.txt'
 char_count = 124
+
 
 with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy_0.1.pkl', 'rb') as fi:
     df_area_01 = pkl.load(fi)
-    with open('files/_ana_data_load_describe.txt', 'w') as fo:
+    with open(output_file_path, 'w') as fo:
         print('-' * char_count, file=fo)
         print('ANALYTICAL DATA OF heuristic_bestfit_area_numpy.py WITH GLOBAL_TIME_LIMIT_PER_ITER = 0.1'.center(111), file=fo)
         print('-' * char_count, file=fo)
@@ -21,7 +23,7 @@ with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy_0.1.pkl', 'r
 
 with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy_1.pkl', 'rb') as fi:
     df_area_1 = pkl.load(fi)
-    with open('files/_ana_data_load_describe.txt', 'a') as fo:
+    with open(output_file_path, 'a') as fo:
         print('-' * char_count, file=fo)
         print('ANALYTICAL DATA OF heuristic_bestfit_area_numpy.py WITH GLOBAL_TIME_LIMIT_PER_ITER = 1'.center(111), file=fo)
         print('-' * char_count, file=fo)
@@ -29,7 +31,7 @@ with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy_1.pkl', 'rb'
 
 with open('files/_analytical_data/_ana_heuristic_bestfit_maxside_numpy_0.1.pkl', 'rb') as fi:
     df_maxside_01 = pkl.load(fi)
-    with open('files/_ana_data_load_describe.txt', 'a') as fo:
+    with open(output_file_path, 'a') as fo:
         print('-' * char_count, file=fo)
         print('ANALYTICAL DATA OF heuristic_bestfit_maxside_numpy.py WITH GLOBAL_TIME_LIMIT_PER_ITER = 0.1'.center(111), file=fo)
         print('-' * char_count, file=fo)
@@ -37,15 +39,38 @@ with open('files/_analytical_data/_ana_heuristic_bestfit_maxside_numpy_0.1.pkl',
 
 with open('files/_analytical_data/_ana_heuristic_bestfit_maxside_numpy_1.pkl', 'rb') as fi:
     df_maxside_1 = pkl.load(fi)
-    with open('files/_ana_data_load_describe.txt', 'a') as fo:
+    with open(output_file_path, 'a') as fo:
         print('-' * char_count, file=fo)
         print('ANALYTICAL DATA OF heuristic_bestfit_maxside_numpy.py WITH GLOBAL_TIME_LIMIT_PER_ITER = 1'.center(111), file=fo)
         print('-' * char_count, file=fo)
         print(df_maxside_1.describe(include='all'), file=fo)
 
-print('Saved to files/_ana_data_load_describe.txt')
+
+print('Described data saved to', output_file_path)
+
+df_area_01.plot(x='rect_count', y=['total cost'])
+plt.savefig('files/_analytical_figures/df_area_01 total_cost')
+df_area_01.plot(x='rect_count', y=['running time'])
+plt.savefig('files/_analytical_figures/df_area_01 running time')
+df_area_01.plot(x='rect_count', y=['time_exceeded_count'])
+plt.savefig('files/_analytical_figures/df_area_01 time_exceeded_count')
 
 df_area_1.plot(x='rect_count', y=['total cost'])
-plt.show()
+plt.savefig('files/_analytical_figures/df_area_1 total_cost')
 df_area_1.plot(x='rect_count', y=['running time'])
-plt.show()
+plt.savefig('files/_analytical_figures/df_area_1 running time')
+
+
+df_maxside_01.plot(x='rect_count', y=['total cost'])
+plt.savefig('files/_analytical_figures/df_maxside_01 total_cost')
+df_maxside_01.plot(x='rect_count', y=['running time'])
+plt.savefig('files/_analytical_figures/df_maxside_01 running time')
+df_maxside_01.plot(x='rect_count', y=['time_exceeded_count'])
+plt.savefig('files/_analytical_figures/df_maxside_01 time_exceeded_count')
+
+df_maxside_1.plot(x='rect_count', y=['total cost'])
+plt.savefig('files/_analytical_figures/df_maxside_1 total_cost')
+df_maxside_1.plot(x='rect_count', y=['running time'])
+plt.savefig('files/_analytical_figures/df_maxside_1 running time')
+
+print('Figures saved to files/_analytical_figures/')
