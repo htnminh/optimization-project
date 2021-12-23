13064eadc7c301332e3ed184d218a96efb87b612
htnminh <nhatminh2106@gmail.com>
2021-12-23 08:26:09 +0700

    edit _ana_data_load_desc to output to txt file

--- files/_ana_data_load_describe.py
+++ files/_ana_data_load_describe.py
@@ -2,10 +2,12 @@ import pickle as pkl
 import pandas as pd
 
 
-with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy.pkl', 'rb') as f:
-    print('-' * 111)
-    print('ANALYTICAL DATA OF heuristic_bestfit_area_numpy.py'.center(111))
-    print('-' * 111)
+with open('files/_analytical_data/_ana_heuristic_bestfit_area_numpy.pkl', 'rb') as fi:
+    df = pkl.load(fi)
+    with open('files/_ana_data_load_describe.txt', 'w') as fo:
+        print('-' * 111, file=fo)
+        print('ANALYTICAL DATA OF heuristic_bestfit_area_numpy.py'.center(111), file=fo)
+        print('-' * 111, file=fo)
+        print(df.describe(), file=fo)
 
-    df = pkl.load(f)
-    print(df.describe())
+print('Saved to files/_ana_data_load_describe.txt')
